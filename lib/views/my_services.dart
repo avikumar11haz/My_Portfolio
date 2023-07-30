39283156779a87c708dddc863109da06df4b8e00
avikumarr355@gmail.com <avikumarr355@gmail.com>
2023-07-30 14:56:08 +0530

    add animations

--- lib/views/my_services.dart
+++ lib/views/my_services.dart
@@ -6,9 +6,15 @@ import 'package:my_portfolio/globals/app_colors.dart';
 import 'package:my_portfolio/globals/app_text_styles.dart';
 import 'package:my_portfolio/globals/constants.dart';
 
-class MyServices extends StatelessWidget {
+class MyServices extends StatefulWidget {
   const MyServices({Key? key}) : super(key: key);
 
+  @override
+  State<MyServices> createState() => _MyServicesState();
+}
+
+class _MyServicesState extends State<MyServices> {
+  bool isApp = false, isGraphic = false, isDataAnalyst = false;
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
@@ -17,7 +23,7 @@ class MyServices extends StatelessWidget {
       height: size.height,
       color: AppColors.bgColor,
       alignment: Alignment.center,
-      padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 12),
+      padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 12),
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
@@ -39,11 +45,43 @@ class MyServices extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              buildAnimatedContainer(),
+              InkWell(
+                  onTap: () {},
+                  onHover: (value) {
+                    setState(() {
+                      isApp = value;
+                    });
+                  },
+                  child: buildAnimatedContainer(
+                      title: 'App Development',
+                      asset: AppAssets.code,
+                      hover: isApp)),
               Constants.sizedBox(width: 18),
-              buildAnimatedContainer(),
+              InkWell(
+                onTap: () {},
+                onHover: (value) {
+                  setState(() {
+                    isGraphic = value;
+                  });
+                },
+                child: buildAnimatedContainer(
+                    title: 'Graphic Designing',
+                    asset: AppAssets.brush,
+                    hover: isGraphic),
+              ),
               Constants.sizedBox(width: 18),
-              buildAnimatedContainer()
+              InkWell(
+                onTap: () {},
+                onHover: (value) {
+                  setState(() {
+                    isDataAnalyst = value;
+                  });
+                },
+                child: buildAnimatedContainer(
+                    title: 'Digital Marketing',
+                    asset: AppAssets.analyst,
+                    hover: isDataAnalyst),
+              )
             ],
           )
         ],
@@ -51,51 +89,50 @@ class MyServices extends StatelessWidget {
     );
   }
 
-  AnimatedContainer buildAnimatedContainer() {
+  AnimatedContainer buildAnimatedContainer(
+      {required String title, required String asset, required bool hover}) {
     return AnimatedContainer(
-              duration: const Duration(milliseconds: 600),
-              width: 400,
-              height: 450,
-              alignment: Alignment.center,
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 18, vertical: 24),
-              decoration: BoxDecoration(
-                  color: AppColors.bgColor2,
-                  borderRadius: BorderRadius.circular(30),
-                  boxShadow: const [
-                    BoxShadow(
-                        color: Colors.black54,
-                        spreadRadius: 4.0,
-                        blurRadius: 4.5,
-                        offset: Offset(3.0, 4.5))
-                  ]),
-              child: Column(
-                children: [
-                  Image.asset(
-                    AppAssets.code,
-                    width: 50,
-                    height: 50,
-                    color: AppColors.themeColor,
-                  ),
-                  Constants.sizedBox(height: 30),
-                  Text(
-                    'App Development',
-                    style: AppTextStyles.montserratStyle(color: Colors.white),
-                  ),
-                  Constants.sizedBox(height: 12),
-                  Text(
-                    'A successful Mobile application developer should be'
-                    'able to demonstrate in the Mobile Application Developer'
-                    'Resume chief skill sets such as – strong programming skills in Java,'
-                    'Objective – C, C#, and Swift, possess experience with API’s and third-party libraries and be familiar with OOP design principles.',
-                    style: AppTextStyles.normalStyle(),
-                    textAlign: TextAlign.center,
-                  ),
-                  Constants.sizedBox(height: 20),
-                  AppButtons.buildMaterialButton(
-                      buttonName: "Read More", onTap: () {})
-                ],
-              ),
-            );
+      duration: const Duration(milliseconds: 600),
+      width: hover ? 400 : 390,
+      height: hover ? 440 : 430,
+      alignment: Alignment.center,
+      padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 24),
+      decoration: BoxDecoration(
+          color: AppColors.bgColor2,
+          borderRadius: BorderRadius.circular(30),
+          boxShadow: const [
+            BoxShadow(
+                color: Colors.black54,
+                spreadRadius: 4.0,
+                blurRadius: 4.5,
+                offset: Offset(3.0, 4.5))
+          ]),
+      child: Column(
+        children: [
+          Image.asset(
+            asset,
+            width: 50,
+            height: 50,
+            color: AppColors.themeColor,
+          ),
+          Constants.sizedBox(height: 30),
+          Text(
+            title,
+            style: AppTextStyles.montserratStyle(color: Colors.white),
+          ),
+          Constants.sizedBox(height: 12),
+          Text(
+            'A successful Mobile application developer should be'
+            'able to demonstrate in the Mobile Application Developer'
+            'Resume chief skill sets such as –'
+            'Objective – C, C#, and Swift, possess experience with API’s and third-party libraries and be familiar with OOP design principles.',
+            style: AppTextStyles.normalStyle(),
+            textAlign: TextAlign.center,
+          ),
+          Constants.sizedBox(height: 20),
+          AppButtons.buildMaterialButton(buttonName: "Read More", onTap: () {})
+        ],
+      ),
+    );
   }
 }
